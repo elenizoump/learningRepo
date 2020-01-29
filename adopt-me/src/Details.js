7cbd1eafc06bdd703bbf491ca1c31fa63b025538
Eleni Zoumpoulaki <elenizoump@hotmail.com>
2020-01-29 19:59:10 +0100

    search results and basic routes

---
+++ adopt-me/src/Details.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+import { Router } from "@reach/router";
+import SearchParams from "./SearchParams";
+
+const Details = () => {
+  return <h1>Hi</h1>;
+};
+
+export default Details;
--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
   const [breed, BreeedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
     setBreed("");
 
     pet.breeds(animal).then(({ breeds }) => {
+      //console.log(breeds);
       const breedStrings = breeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error);
@@ -19,7 +33,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form action="">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">Location</label>
         <input
           id="location"
@@ -31,6 +50,7 @@ const SearchParams = () => {
         <BreeedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
